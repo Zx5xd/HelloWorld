f13a2f08ca3e434aa51e78202dfdad9c9f65a770
DESKTOP-KDMLA8O\ddal_ <wvw201016@gmail.com>
2022-03-16 10:53:14 +0900

    파이썬 기초강의

---
+++ PythonIDle/python 기초 강의/Graphic_turtle.py
@@ -0,0 +1,20 @@
+## 파이썬 터틀 모듈
+import turtle
+
+## 캔버스를 불러온다.
+t = turtle.Pen()
+
+## 마우스 커버 변경
+t.shape("turtle")
+
+## 펜 색깔 변경
+t.pencolor("blue")
+
+## 직선 100 픽셀
+t.forward(100)
+
+## 마우스 커버 방향(오른쪽 90도)
+t.right(90)
+
+
+
