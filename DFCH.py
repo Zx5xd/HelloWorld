013ab3be137221b87cf1928f068b72a2a4c28e99
Zetia <101085187+Zx5xd@users.noreply.github.com>
2022-07-22 23:38:01 +0900

    20220722

--- DFCH.py
+++ DFCH.py
@@ -12,47 +12,38 @@ def selc():
     filename = askopenfilenames(filetypes=(("webp, jpg, gif 파일", "*.webp *.jpg *.gif"),
                                            ("jpg 파일", "*.jpg"), ("gif 파일", "*.gif"), ("webp 파일","*.webp"),
                                            ("모든 파일", "*.*")))
-    ch = 0
-    func(ch)
+    func()
 
 
-def func(ch):
+def func():
     for i in range(0, len(filename)):
-        print(os.path.basename(filename[i]))
+        #print(os.path.basename(filename[i]))
         name, ext = os.path.splitext(filename[i])
-        if(ch == 0):
+
+        basename = os.path.basename(name)
+        m = re.findall('\d',basename)
+        print(basename)
+
+        if(len(m)==1):
+            basename = basename.replace(m[0], "0" + m[0])
             os.renames(filename[i],
-                   os.path.join(os.path.dirname(filename[i]),name+"."+e.get()))
+            os.path.join(os.path.dirname(filename[i]),basename+"."+e.get()))
         else:
-            m = re.findall('\d',os.path.basename(name))
-            if(len(m)>1):
-                str = ""
-                for j in range(0, len(m)):
-                    str += m[j]
-            elif(len(m)==1):
-                str = "0"+m
-            else:
-                continue
-
-
+            os.renames(filename[i],
+                       os.path.join(os.path.dirname(filename[i]), name + "." + e.get()))
 
 
 def ent(event):
     selc()
 
-def Az():
-    ch = 1
-    func(ch)
 
 
     
 
 tk = Tk()
-label = Label(tk, text="변경할 확장자").grid(column=0, row=0)
 
 e = Entry(tk)
 e.grid(column=0, row=1)
 e.bind('<Return>',ent)
 but = Button(tk, command=selc, text="Click").grid(column=1, row=1)
-but2 = Button(tk, command=Az, text="Zero+").grid(column=1,row=2)
 tk.mainloop()
